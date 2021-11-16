04d503a606babdbe12a284659aac7e4307d0dca1
wagnerkaba <bom.link+github@gmail.com>
2021-11-16 13:37:58 -0300

    First commit

---
+++ src/test/java/com/wagner/servicediscorvery/ServiceDiscorveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagner.servicediscorvery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscorveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
